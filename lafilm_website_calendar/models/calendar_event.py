b3fab9e9633c4aef0c943053f6dd8d4986ff80f0
DShah-Adalogic <dshah@adalogic.co>
2020-04-13 09:37:49 -0700

    [ADD] Temporarily made Online appointment not readonly.

--- lafilm_website_calendar/models/calendar_event.py
+++ lafilm_website_calendar/models/calendar_event.py
@@ -8,5 +8,5 @@ from odoo import api, fields, models
 class CalendarEvent(models.Model):
     _inherit = "calendar.event"
 
-    appointment_type_id = fields.Many2one('calendar.appointment.type', 'Online Appointment')
+    appointment_type_id = fields.Many2one('calendar.appointment.type', 'Online Appointment', readonly=False)
 
